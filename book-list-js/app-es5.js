e343e742e38147f86e736f02045d39956c1479c1
Cory Gray <crgray21@yahoo.com>
2019-07-10 21:49:51 -0400

    event listeners and constructors

---
+++ book-list-js/app-es5.js
@@ -0,0 +1,17 @@
+// Book Constructor
+function Book(title, author, isbn) {
+    this.title = title;
+    this.author = author;
+    this.isbn = isbn;
+}
+
+// UI Constructor
+function UI() {}
+
+// Event listeners
+document.getElementById('book-form').addEventListener('submit', function(e) {
+    const title = document.getElementById('title').value;
+    const author = document.getElementById('author').value;
+    const isbn = document.getElementById('isbn').value;
+    e.preventDefault();
+});
