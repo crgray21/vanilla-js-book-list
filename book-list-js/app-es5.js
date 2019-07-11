8d643277c0ab8d20dbbaf4bf91dcd10f8c01b3ca
Cory Gray <crgray21@yahoo.com>
2019-07-10 22:09:37 -0400

    UI functions addBook and clearFields

--- book-list-js/app-es5.js
+++ book-list-js/app-es5.js
@@ -8,10 +8,39 @@ function Book(title, author, isbn) {
 // UI Constructor
 function UI() {}
 
+// UI prototype functions
+UI.prototype.addBookToList = function(book) {
+    const list = document.getElementById('book-list');
+    const row = document.createElement('tr');
+    row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="delete-row">X</td>
+        `;
+
+    list.appendChild(row);
+}
+
+UI.prototype.clearFields = function() {
+    document.getElementById('title').value = '';
+    document.getElementById('author').value = '';
+    document.getElementById('isbn').value = '';
+
+}
+
+
 // Event listeners
 document.getElementById('book-form').addEventListener('submit', function(e) {
     const title = document.getElementById('title').value;
     const author = document.getElementById('author').value;
     const isbn = document.getElementById('isbn').value;
+
+    const book = new Book(title, author, isbn);
+    const ui = new UI();
+
+    ui.addBookToList(book); 
+    ui.clearFields();
+
     e.preventDefault();
 });
